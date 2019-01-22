29dd8dbaec5590f5284e0f96de82285c784b4726
dencho <tsanov.dencho@gmail.com>
2019-01-22 08:41:14 +0200

    modify

---
+++ Module.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dencho Tsanov
+ * Date: 22.10.18 22:27
+ * File: Module.php
+ *
+ * Project: yii2-cms-admin
+ *
+ */
+
+namespace denchotsanov;
+
+
+class Module
+{
+
+}
---
+++ controller/SiteController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dencho Tsanov
+ * Date: 22.10.18 22:18
+ * File: SiteController.php
+ *
+ * Project: yii2-cms-admin
+ *
+ */
+
+namespace denchotsanov\controller;
+
+
+class SiteController
+{
+
+}
